7ef34158b854023411d1a9b6e2ed8109c98cab63
Urooj Ahmad <urooz2002@gmail.com>
2023-08-18 21:13:26 +0530

    few changes

--- script.js
+++ script.js
@@ -95,8 +95,8 @@ function editItem(button) {
 }
 
 function clearStorage() {
-    localStorage.clear();
-    updateList(); 
+    itemList.innerHTML = ''; 
+    localStorage.clear();    
 }
 
 function updateList() {
